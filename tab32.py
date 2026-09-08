1e2074c62b04760355c2bb32498f3af3d339e623
mirzatabrezaltamashbaig2006-creator <mirzatabrezaltamashbaig2006@gmail.com>
2026-09-08 11:25:27 +0530

    Add files via upload

---
+++ tab32.py
@@ -0,0 +1,10 @@
+n = 14
+if n > 0:
+    if n % 2 == 0:
+        print("Positive and Even")
+    else:
+        print("Positive and Odd")
+elif n < 0:
+    print("Negative")
+else:
+    print("Zero")
