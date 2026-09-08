a38e871838278dec0d1dc4c11e1d8d7ebb79151f
mirzatabrezaltamashbaig2006-creator <mirzatabrezaltamashbaig2006@gmail.com>
2026-09-08 10:33:20 +0530

    Add files via upload

---
+++ tab8.py
@@ -0,0 +1,8 @@
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+c = float(input("Enter third number: "))
+
+if a == b and b == c:
+    print("All three numbers are equal")
+else:
+    print("All three numbers are not equal")
