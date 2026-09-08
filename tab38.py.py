803980e5f8d020f6f95caa727aade5e43b0c6bd7
mirzatabrezaltamashbaig2006-creator <mirzatabrezaltamashbaig2006@gmail.com>
2026-09-08 11:46:37 +0530

    Add files via upload

---
+++ tab38.py.py
@@ -0,0 +1,12 @@
+num = int(input("Enter a 3-digit number: "))
+
+a = num // 100
+b = (num // 10) % 10
+c = num % 10
+
+sum = a**3 + b**3 + c**3
+
+if sum == num:
+    print("Armstrong number")
+else:
+    print("Not an Armstrong number")
