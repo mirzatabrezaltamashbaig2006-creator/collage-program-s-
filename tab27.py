3b9cc58714cb03634ba8efd397b304546254e5eb
mirzatabrezaltamashbaig2006-creator <mirzatabrezaltamashbaig2006@gmail.com>
2026-09-08 11:13:52 +0530

    Add files via upload

---
+++ tab27.py
@@ -0,0 +1,9 @@
+units = 250
+bill = 0
+if units <= 100:
+    bill = units * 2
+elif units <= 200:
+    bill = (100 * 2) + (units - 100) * 3
+else:
+    bill = (100 * 2) + (100 * 3) + (units - 200) * 5
+print(f"Total Bill: ₹{bill}")
