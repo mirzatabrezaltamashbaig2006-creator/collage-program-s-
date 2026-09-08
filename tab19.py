95bcdc6be0a26f13baa62115169afed00b8ff06a
mirzatabrezaltamashbaig2006-creator <mirzatabrezaltamashbaig2006@gmail.com>
2026-09-08 10:50:39 +0530

    Add files via upload

---
+++ tab19.py
@@ -0,0 +1,10 @@
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+c = float(input("Enter third number: "))
+
+if a >= b and a >= c:
+    print("Largest number =", a)
+elif b >= a and b >= c:
+    print("Largest number =", b)
+else:
+    print("Largest number =", c)
