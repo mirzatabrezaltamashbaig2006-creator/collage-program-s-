51bf8f419353269f9bc264c62e1670104b8230fe
mirzatabrezaltamashbaig2006-creator <mirzatabrezaltamashbaig2006@gmail.com>
2026-09-08 11:45:35 +0530

    Add files via upload

---
+++ tab37.py.py
@@ -0,0 +1,14 @@
+
+x = float(input("Enter x: "))
+y = float(input("Enter y: "))
+
+if x > 0 and y > 0:
+    print("First Quadrant")
+elif x < 0 and y > 0:
+    print("Second Quadrant")
+elif x < 0 and y < 0:
+    print("Third Quadrant")
+elif x > 0 and y < 0:
+    print("Fourth Quadrant")
+else:
+    print("Point lies on an axis")
