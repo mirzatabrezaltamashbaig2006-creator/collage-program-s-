75cd283127280eecfc1550f94c58e8e122ae579a
mirzatabrezaltamashbaig2006-creator <mirzatabrezaltamashbaig2006@gmail.com>
2026-09-08 10:29:33 +0530

    Add files via upload

---
+++ tab6.py
@@ -0,0 +1,9 @@
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+if a > b:
+    print("Greater number =", a)
+elif b > a:
+    print("Greater number =", b)
+else:
+    print("Both numbers are equal")
