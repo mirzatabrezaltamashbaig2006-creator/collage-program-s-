ddc1d11397f2a0aee46f4483b571936b92df4761
mirzatabrezaltamashbaig2006-creator <mirzatabrezaltamashbaig2006@gmail.com>
2026-09-08 10:49:28 +0530

    Add files via upload

---
+++ tab18.py
@@ -0,0 +1,9 @@
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+if a > b:
+    print("Largest number =", a)
+elif b > a:
+    print("Largest number =", b)
+else:
+    print("Both numbers are equal")
